d2d6f622e4467ca7eb3a573fbc05f7d66f0df5c8
lazubic <laura@zubic.com>
2022-12-12 16:07:41 +0100

    Datenstruktur angelegt

---
+++ code/src/components/ListHabits.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-for="habit in habits">{{ habit.title }}, {{ habit.ready }}</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const habits = ref([
+  { title: "Sport machen", ready: false },
+  { title: "Duschen", ready: false },
+  { title: "Trinken", ready: false },
+  { title: "Essen", ready: false },
+]);
+</script>
